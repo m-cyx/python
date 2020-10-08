1dfcc8d5baf0c8e874ab6e26ae8d4cb00cb33a40
m-cyx <markpixel05@gmai.com>
2020-10-08 09:21:45 +0300

    доьавио разделители строк

--- Text Analysis/lab1.py
+++ Text Analysis/lab1.py
@@ -6,7 +6,7 @@ from nltk import download
 from nltk.corpus import stopwords
 
 # Открытие файла на чтение
-f = io.open('text.txt', encoding='utf-8')
+f = io.open('text1.txt', encoding='utf-8')
 text = f.read()
 
 # Получаю список предложений и список слов, стоп слов
@@ -16,10 +16,13 @@ stop_words = set(stopwords.words("russian"))
 
 # Запись в файл построчно предложения
 f = io.open('result.txt', 'w', encoding='utf-8')
+f.write('Токенизация по предложениям:' + '\n\n')
 for i in sentences:
     f.write(str(i) + '\n')
 # Открываю на дозапись слов и их множества без стоп слов 
 f = io.open('result.txt', 'a', encoding='utf-8')
+
+f.write( '\n' + 'Токенизация слов:' + '\n')
 f.write(str(words) + '\n')
 nostop = []
 # Иду по словам, если слово не в стоп листе, то добавляю к списку слов
@@ -27,6 +30,8 @@ for i in words:
     if i not in stop_words:
         nostop.append(i)
 # Записываю в файл множество слов из  списка 
+
+f.write('\n' + 'Множество слов без стоп слов:' + '\n')
 f.write(str(set(nostop)))
 
 f.close
