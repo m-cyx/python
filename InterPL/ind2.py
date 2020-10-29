cf7ba171892e8a6f7acc30d8d0135d54863141f3
m-cyx <markpixel05@gmai.com>
2020-10-29 20:41:32 +0300

    update ind 2

--- InterPL/ind2.py
+++ InterPL/ind2.py
@@ -2,19 +2,39 @@
 Выборы в США - 2
 """
 states = {}
+listofstates = []
 N = int(input('Введите кол-во штатов: '))
 for i in range(N):
     s = input('Введите штат и кол-во выборщиков: ')
     s = s.split()
     states [s[0]] = int(s[1])
+    listofstates.append(s[0])
 
 print(states)
 
-results = {}
-counter = 0
-while counter < 4:
-    r = input('штат - избиратель: ')
-    r.split()
-    results [r[0]] = r[1]
-    counter += 1
-print (results)
+results = []
+dresults = {}
+tmp = input('штат - избиратель: ')  # тут нужна проверка, что голосует не больше людей, чем в штате выборщиков 
+while tmp != '0':
+    stmp = tmp.split()
+    results.append(stmp)
+    dresults [stmp[0]] = stmp[1]
+    tmp = input('штат - избиратель: ')
+print(results)
+print(dresults)
+list1 = []
+list2 = []
+"""
+for el in results:
+    for el1 in el:
+        if el1 == listofstates[0]:
+            list1.append(el)
+"""
+for el in results:
+    if el[0] == listofstates[0]:
+        list1.append(el)
+print(list1)
+print(list2)
+
+# пройти по списку и если первый элемент в паре равен первому элементу из списка штатов, 
+# то добавить эту пару в list1 и тд для кол-ва штатов 
