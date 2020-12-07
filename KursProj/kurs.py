ec6c1c854e63249860c6e21d9efc52a96fc7dfc4
m-cyx <markpixel05@gmai.com>
2020-12-07 15:38:57 +0300

    update

--- KursProj/kurs.py
+++ KursProj/kurs.py
@@ -27,43 +27,49 @@ for line in text:
 
 colorcode = [line.rstrip() for line in colorcode] # удаляю служебные \n которые появились при f.readlines()
 colorname = [line.rstrip() for line in colorname]
-# тут привожу имена цветов в нормальную форму
+
+# Привожу имена цветов в нормальную форму
 for el in colorname:
     p = morph.parse(el)[0] 
     normalcolorname.append(str(p.normal_form))
+
+# "Упаковываю" названия и цвета в словарь
 colorbook = dict(zip(normalcolorname, colorcode))
 #print(colorbook)
 f.close()
+
 #################################################################################################
-# в этой части беру текст, отчищаю от стоп слов и привожу в начальную форму. На выходе должен быть список слов.
+# В этой части беру текст, отчищаю от стоп слов и привожу в начальную форму. На выходе должен быть список слов.
 
 # Открытие файла на чтение
-f = io.open('text2.txt', encoding='utf-8')
+f = io.open('text3.txt', encoding='utf-8')
 text = f.read()
 
-# получение списка слов и стоп слов
+# Получение списка слов и стоп слов
 words = word_tokenize(text)
 stop_words = set(stopwords.words("russian"))
 nostop = []
+
 # Иду по словам, если слово не в стоп листе, то добавляю к списку слов
 for i in words:
     if i not in stop_words:
         nostop.append(i.lower())
-# убираю знаки пунктуации
+
+# Убираю знаки пунктуации
 punctuation = ['(', ')', '?', ':', ';', ',', '.', '!', '/', '"', "'"]
 nostop = [item for item in nostop if item not in punctuation]
 #print(nostop)
 
-#########################################################################################################
-# тут пробую найти соответствия
+###################################################################################################
+# Инициализирую молфологический анализатор
 import pymorphy2
 morph = pymorphy2.MorphAnalyzer()
 
+# Привожу 
 normalnostop = []
 for el in nostop:
     p = morph.parse(el)[0] 
     normalnostop.append(str(p.normal_form))
-#print('******************************************************************')
 #print(normalnostop)
 
 for key in colorbook:
@@ -71,16 +77,35 @@ for key in colorbook:
         print(key + '->' + colorbook[key])
 
 print('*************************** ПОЛНЫЙ СПИСОК ВСЕХ ЦВЕТОВ *********************')
+
 allcolors = []
 for key in normalnostop:
     if key in colorbook:
-        print(key + '   ->  ' + colorbook[key])
-        allcolors.append(key + '   ->  ' + colorbook[key])
+        #print(key + '   ->  ' + colorbook[key])
+        allcolors.append(key + '\n->  ' + colorbook[key] + '\n')
+
+
+
+from collections import Counter
+
+c = Counter(allcolors)
+
+for key in c:
+    print(key + '       Кол-во: ' + str(c[key]) + '\n')
 
 '''
 можно выводить результат в html с цветами
 или в tkinter
-'''
-'''
-попробовать посчитать частоту каждого цвета 
+
+
+
+рекомендовать последовательность чтения книг 
+по нарастанию позитианой тональности и спектрограммы 
+связать настрой человека с цветовой гаммой 
+
+переход от спектрограммы к анализу тональности текста 
+
+
+спич ту текст и цвет 
+
 '''
