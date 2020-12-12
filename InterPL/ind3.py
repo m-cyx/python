5dbc490653c375242412694722afc26ff1b9b2cf
m-cyx <markpixel05@gmai.com>
2020-12-12 16:15:01 +0300

    решил задачу на словари

--- InterPL/ind3.py
+++ InterPL/ind3.py
@@ -1,11 +1,26 @@
+'''
+Вводятся пары столица - государство, их надо сортировать по государству
+'''
+# Инициализация
 n = int(input('Введите кол-во строк: '))
 dict = {}
-d_list = []
+gos_list = []
+
+# Ввод словаря и списка государств
 for i in range(n):
     s = input('Введите столицу и государство: ')
     s = s.split()
-    dict [s[1]] = s[0]
-    d_list.append(s)
+    dict [s[0]] = s[1]
+    gos_list.append(s[1])
 
-print(dict)
-print(d_list) # пары столица - государство, их надо сортировать по государству
+# Сортированный список государств
+gos_list = sorted(gos_list) 
+
+# Вывожу исходный словарь
+print(dict)   
+
+# Вывод отсортированных пар
+for el in gos_list:
+    for key in dict:
+        if dict[key] == el:
+            print(key + ' ' + dict[key])
