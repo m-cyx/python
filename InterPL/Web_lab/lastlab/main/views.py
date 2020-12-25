379daee2efe08efb8a00d0c6dee5d1df602d7276
m-cyx <markpixel05@gmai.com>
2020-12-26 02:36:55 +0300

    update

---
+++ InterPL/Web_lab/lastlab/main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse('<h4>hello</h4>')
+
+
+def about(request):
+    return HttpResponse('<h4>about</h4>')
