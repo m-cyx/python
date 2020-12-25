2dd1fa51b836c979cd7aaccd2410d0fe2556056c
m-cyx <markpixel05@gmai.com>
2020-12-25 09:33:10 +0300

    update

--- InterPL/CgiLab/importLab.py
+++ InterPL/CgiLab/importLab.py
@@ -1,1 +1,33 @@
-from xml.dom import minidom
+import xml.etree.ElementTree as xml
+ 
+
+"""
+Создаем XML файл.
+"""
+root = xml.Element("zAppointments")
+appt = xml.Element("appointment")
+root.append(appt)
+    
+# создаем дочерний суб-элемент. 
+begin = xml.SubElement(appt, "begin")
+begin.text = "1181251680"
+
+uid = xml.SubElement(appt, "uid")
+uid.text = "040000008200E000"
+
+alarmTime = xml.SubElement(appt, "alarmTime")
+alarmTime.text = "1181572063"
+
+state = xml.SubElement(appt, "state")
+
+location = xml.SubElement(appt, "location")
+
+duration = xml.SubElement(appt, "duration")
+duration.text = "1800"
+
+subject = xml.SubElement(appt, "subject")
+
+tree = xml.ElementTree(root)
+with open("appt.xml", "w") as fh:
+    tree.write(fh)
+ 
